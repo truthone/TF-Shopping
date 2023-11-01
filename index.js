7ceca11a818d26c193fb739f1081cd60ef802fb5
jungAh Han <truth.h406@gmail.com>
2023-11-01 19:24:58 +0900

    add: 검색바 추가, 정적데이터안에서 찾기, 사과 클릭하면 상세페이지 이동

---
+++ index.js
@@ -0,0 +1,39 @@
+const searchInput = document.getElementById("searchInput");
+const searchResults = document.getElementById("searchResults");
+const data = [
+  { name: "사과", link: "./apple.html" },
+  { name: "바나나" },
+  { name: "체리" },
+  { name: "자두" },
+  { name: "포도" },
+  // 데이터 예시
+];
+
+searchInput.addEventListener("input", performSearch);
+
+// 검색 함수 구현
+function performSearch() {
+  const searchTerm = searchInput.value.toLowerCase();
+
+  // 데이터를 필터링하여 검색 결과 어레이 생성
+  const filteredData = data.filter((item) =>
+    item.name.toLowerCase().includes(searchTerm)
+  );
+
+  // 검색 결과를 화면에 표시
+  renderResults(filteredData);
+}
+
+// 검색 결과를 화면에 렌더링하는 함수
+function renderResults(results) {
+  searchResults.innerHTML = "";
+
+  results.map((item) => {
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+    a.href = item.link;
+    a.textContent = item.name;
+    li.appendChild(a);
+    searchResults.appendChild(li);
+  });
+}
