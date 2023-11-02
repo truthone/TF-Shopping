03bb8f2673f253943b2e9162eeb22d848b3426a8
jungAh.Han <truth.h406@gmail.com>
2023-11-02 21:05:08 +0900

    delete: 주석

--- index.js
+++ index.js
@@ -6,28 +6,25 @@ const data = [
   { name: "체리" },
   { name: "자두" },
   { name: "포도" },
-  // 데이터 예시
 ];
 
 searchInput.addEventListener("input", performSearch);
 
-// 검색 함수 구현
 function performSearch() {
   const searchTerm = searchInput.value.toLowerCase();
   if (searchTerm && searchTerm != '') {
     searchResults.style.display = 'block';
-    // 데이터를 필터링하여 검색 결과 어레이 생성
+
     const filteredData = data.filter((item) =>
       item.name.toLowerCase().includes(searchTerm)
     );
-    // 검색 결과를 화면에 표시
+
     renderResults(filteredData);
   } else {
     searchResults.style.display = 'none';
   }
 }
 
-// 검색 결과를 화면에 렌더링하는 함수
 function renderResults(results) {
   searchResults.innerHTML = "";
 
