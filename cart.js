ccf76c516d3adacc59c4259d9b7b431e6a4318db
jungAh.Han <truth.h406@gmail.com>
2023-11-02 23:34:30 +0900

    add: localstorage 이용한 장바구니 긴으

--- cart.js
+++ cart.js
@@ -1,3 +1,41 @@
-function addCart() {
-  window.alert("장바구니에 추가 되었습니다.");
+let cart = JSON.parse(localStorage.getItem('cart')) || [];
+
+function addToCart(item) {
+    cart.push(item);
+    saveCartToLocalStorage();
+    displayCart();
 }
+
+function removeFromCart(index) {
+    cart.splice(index, 1);
+    saveCartToLocalStorage();
+    displayCart();
+}
+
+function saveCartToLocalStorage() {
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+function displayCart() {
+    const cartList = document.getElementById('cartList');
+    cartList.innerHTML = '';
+
+    for(let i = 0; i < cart.length; i++) {
+        let item = document.createElement('li');
+        item.textContent = cart[i];
+        
+        let removeButton = document.createElement('button');
+        removeButton.textContent = '제거';
+        (function(index) {
+            removeButton.addEventListener('click', function() {
+                removeFromCart(index);
+            });
+        })(i);
+
+        item.appendChild(removeButton);
+        cartList.appendChild(item);
+    }
+}
+
+// 처음 페이지 로드 시 장바구니 출력
+displayCart();
