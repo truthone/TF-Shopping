31333078c55edc28ebd56abe0506fb46158eb591
jungAh.Han <truth.h406@gmail.com>
2023-11-02 21:59:49 +0900

    update: 포커스 아웃 될떄 사라지기, 포커스 되고 빈문자열이면 사라지기

--- index.js
+++ index.js
@@ -10,15 +10,25 @@ const data = [
 
 searchInput.addEventListener("input", performSearch);
 
+searchInput.addEventListener('focus', function () {
+  if (searchInput.value !== '') {
+    searchResults.style.display = 'block';  
+  }
+});
+// searchInput에서 포커스가 벗어났을 때
+searchInput.addEventListener('blur', function () {
+    searchResults.style.display = 'none';
+});
+
 function performSearch() {
   const searchTerm = searchInput.value.toLowerCase();
+  
   if (searchTerm && searchTerm != '') {
     searchResults.style.display = 'block';
 
     const filteredData = data.filter((item) =>
       item.name.toLowerCase().includes(searchTerm)
     );
-
     renderResults(filteredData);
   } else {
     searchResults.style.display = 'none';
